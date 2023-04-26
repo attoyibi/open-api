2b12d51f1ef9e78493d058c28dbd621b538faa02
attoyibi <muchson@alterra.id>
2023-04-26 12:21:51 +0700

    bnasic prompt

--- README.md
+++
@@ -1,2 +0,0 @@
-# open-ai
-berisikan contoh kode untuk penggunaan open.ai 
--- basic-prompt/src/App.js
+++ basic-prompt/src/App.js
@@ -18,7 +18,9 @@ function App() {
       const response = await openai.createCompletion({
         model: "text-davinci-003",
         // model: "gpt-3.5-turbo",
-        prompt: prompt,
+        prompt:
+          "A table summarizing the fruits from Goocrux:\n\nThere are many fruits that were found on the recently discovered planet Goocrux. There are neoskizzles that grow there, which are purple and taste like candy. There are also loheckles, which are a grayish blue fruit and are very tart, a little bit like a lemon. Pounits are a bright green color and are more savory than sweet. There are also plenty of loopnovas which are a neon pink flavor and taste like cotton candy. Finally, there are fruits called glowls, which have a very sour and bitter taste which is acidic and caustic, and a pale orange tinge to them.\n\n| Fruit | Color | Flavor |" +
+          prompt,
         temperature: 0.5,
         max_tokens: 100,
       });
